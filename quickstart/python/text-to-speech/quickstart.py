9c9d391bc9e1e8848aa0575a8316191066dc26e2
FreeAaron <jason730lee@gmail.com>
2022-09-26 01:07:54 +0800

    測試OK 也有轉wav

--- quickstart/python/text-to-speech/quickstart.py
+++ quickstart/python/text-to-speech/quickstart.py
@@ -6,32 +6,46 @@ import azure.cognitiveservices.speech as speechsdk
 
 # Creates an instance of a speech config with specified subscription key and service region.
 # Replace with your own subscription key and service region (e.g., "westus").
-speech_key, service_region = "YourSubscriptionKey", "YourServiceRegion"
-speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+speech_key, service_region = "YourSubscriptionKey", "eastus"
+speech_config = speechsdk.SpeechConfig(
+    subscription=speech_key, region=service_region)
 
 # Set the voice name, refer to https://aka.ms/speech/voices/neural for full list.
-speech_config.speech_synthesis_voice_name = "en-US-AriaNeural"
+speech_config.speech_synthesis_voice_name = "en-GB-LibbyNeural"
 
 # Creates a speech synthesizer using the default speaker as audio output.
 speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
 
 # Receives a text from console input.
 print("Type some text that you want to speak...")
-text = input()
+# text = input()
+text = "The stars above are constantly changing, but usually these changes are too slow or too faint for the eye to see. Your challenge is to develop a learning tool to teach people about stellar variability and help them understand how dynamic the night sky really is!"
 
 # Synthesizes the received text to speech.
 # The synthesized speech is expected to be heard on the speaker with this line executed.
 result = speech_synthesizer.speak_text_async(text).get()
 
-# Checks result.
-if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-    print("Speech synthesized to speaker for text [{}]".format(text))
-elif result.reason == speechsdk.ResultReason.Canceled:
-    cancellation_details = result.cancellation_details
-    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
-    if cancellation_details.reason == speechsdk.CancellationReason.Error:
-        if cancellation_details.error_details:
-            print("Error details: {}".format(cancellation_details.error_details))
-    print("Did you update the subscription info?")
-# </code>
+speech_config.set_speech_synthesis_output_format(
+    speechsdk.SpeechSynthesisOutputFormat.Riff24Khz16BitMonoPcm)
+synthesizer = speechsdk.SpeechSynthesizer(
+    speech_config=speech_config, audio_config=None)
 
+result = synthesizer.speak_text_async(
+    "The stars above are constantly changing, but usually these changes are too slow or too faint for the eye to see. Your challenge is to develop a learning tool to teach people about stellar variability and help them understand how dynamic the night sky really is!").get()
+stream = speechsdk.AudioDataStream(result)
+# stream.save_to_wav_file("path/to/write/file.wav")
+stream.save_to_wav_file("speech.wav")
+
+# Checks result.
+# if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+#     print(
+#         "Speech synthesized to speaker for text [{}]".format(text))
+# elif result.reason == speechsdk.ResultReason.Canceled:
+#     cancellation_details = result.cancellation_details
+#     print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+#     if cancellation_details.reason == speechsdk.CancellationReason.Error:
+#         if cancellation_details.error_details:
+#             print("Error details: {}".format(
+#                 cancellation_details.error_details))
+#     print("Did you update the subscription info?")
+# </code>
